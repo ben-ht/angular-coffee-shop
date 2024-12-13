bf6a41cc471dccc7a156c319dbdda90ee182ad8a
Benjamin HEMBERT <benjamin.hembert@orange.com>
2024-12-13 16:00:14 +0100

    Added router import

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { SearchBarComponent } from "../search-bar/search-bar.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule, MatButtonModule, SearchBarComponent],
+  imports: [MatIconModule, MatButtonModule, SearchBarComponent, RouterModule],
 
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
