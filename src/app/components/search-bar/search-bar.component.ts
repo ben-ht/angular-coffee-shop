d87ccd21a49a530871a788c9d6a6d4b7ac149bcf
Benjamin HEMBERT <benjamin.hembert@orange.com>
2024-12-13 14:26:15 +0100

    Initial commit

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [MatIconModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css'
+})
+export class SearchBarComponent {
+
+}
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [MatButtonModule],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent {
+
+}
